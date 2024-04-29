433f2f1620479d99bc3dac66786c006659977051
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-04-29 12:28:24 -0600

    Simple MUI navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import { green, purple } from "@mui/material/colors";
+import { MuiNavbar } from "./components/MuiNavbar";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
 export default function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <ThemeProvider theme={theme}>
+        <MuiNavbar />
+      </ThemeProvider>
     </>
   );
 }
